5ea385ac0c2bad1d46812170f5bff3be8b4db7e8
huweihuang <huweihuang@foxmail.com>
2022-09-05 18:43:34 +0800

    add install etcd scripts

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# deploy-etcd
-deploy-etcd
+# install etcd
+
+## 无证书安装
+
+```bash
+chmod +x install-etcd.sh
+
+# 在每台机器上以下命令，-m 表示当前执行机器IP
+./install-etcd.sh -a <ip1> -b <ip2> -c <ip3> -m <ip1> -n "etcd01" -v <version> -d <etcd-data-dir>
+```
+
+查看部署状态
+
+```
+etcdctl --endpoints=<ip1>:2379,<ip3>:2379,<ip3>:2379 endpoint status -w table
+
+etcdctl --endpoints=<ip1>:2379,<ip3>:2379,<ip3>:2379 put /test test
+
+etcdctl --endpoints=<ip1>:2379,<ip3>:2379,<ip3>:2379 get /test
+```
+
+## 有证书安装
+
+> TODO
+
+# troubleshooting
+
+```json
+{"level":"warn","ts":"2022-09-03T18:30:17.536+0800","caller":"embed/config_logging.go:169","msg":"rejected connection","remote-addr":"1.1.1.1:58824","server-name":"","error":"tls: failed to verify client certificate: x509: certificate signed by unknown authority"}
+```
+
---
+++ install-etcd.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+set -e
+set -x
+
+ETCD_VER=${ETCD_VER:-v3.5.3}
+ETCD_NAME=${ETCD_NAME:-"etcd01"}
+ETCD_DATA_DIR=${ETCD_DATA_DIR:-"/data/etcd"}
+
+NODE1_IP=
+NODE2_IP=
+NODE3_IP=
+Master_IP=
+
+while getopts ":a:b:c:m:v:n:d:" opt
+do
+    case $opt in
+        a) NODE1_IP=$OPTARG
+        echo "NODE1_IP value: $OPTARG"
+        ;;
+        b) NODE2_IP=$OPTARG
+        echo "NODE1_IP value: $OPTARG"
+        ;;
+        c) NODE3_IP=$OPTARG
+        echo "NODE1_IP value: $OPTARG"
+        ;;
+        m) Master_IP=$OPTARG
+        echo "M_IP value: $OPTARG"
+        ;;        
+        v) ETCD_VER=$OPTARG
+        echo "ETCD_VER value: $OPTARG"
+        ;;
+        n) ETCD_NAME=$OPTARG
+        echo "ETCD_NAME value: $OPTARG"
+        ;;
+        d) ETCD_DATA_DIR=$OPTARG
+        echo "ETCD_DATA_DIR value: $OPTARG"
+        ;;                
+        ?)
+        echo "invalid arg"
+        exit 1;;
+    esac
+done
+
+
+# clean
+rm -fr etcd-${ETCD_VER}-linux-amd64.tar.gz etcd-${ETCD_VER}-linux-amd64
+rm -fr /lib/systemd/system/etcd.service /etc/systemd/system/etcd.service
+rm -fr /usr/local/bin/etcd*
+rm -fr ${ETCD_DATA_DIR}
+
+# download etcd bin
+wget http://github.com/etcd-io/etcd/releases/download/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz
+tar -zvxf etcd-${ETCD_VER}-linux-amd64.tar.gz
+cp -fr etcd-${ETCD_VER}-linux-amd64/etcd* /usr/local/bin/
+
+# add etcd serivce
+cat > /lib/systemd/system/etcd.service << EOF
+[Unit]
+Description=Etcd Server
+After=network.target
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+Type=notify
+EnvironmentFile=-/etc/etcd/etcd.conf
+ExecStart=/usr/local/bin/etcd
+Restart=on-failure
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# add etcd conf
+mkdir -p /etc/etcd
+cat > /etc/etcd/etcd.conf << EOF
+ETCD_NAME="${ETCD_NAME}"
+ETCD_DATA_DIR="${ETCD_DATA_DIR}"
+
+# Initial cluster configuration
+ETCD_INITIAL_CLUSTER="etcd01=http://${NODE1_IP}:2380,etcd02=http://${NODE2_IP}:2380,etcd03=http://${NODE3_IP}:2380"
+ETCD_INITIAL_CLUSTER_TOKEN="etcd-cluster"
+ETCD_INITIAL_CLUSTER_STATE="new"
+
+# Peer configuration
+ETCD_INITIAL_ADVERTISE_PEER_URLS="http://${Master_IP}:2380"
+ETCD_LISTEN_PEER_URLS="http://${Master_IP}:2380"
+
+# Client configuration
+ETCD_ADVERTISE_CLIENT_URLS="http://${Master_IP}:2379"
+ETCD_LISTEN_CLIENT_URLS="http://${Master_IP}:2379,http://127.0.0.1:2379"
+EOF
+
+systemctl daemon-reload
+systemctl enable etcd
+systemctl restart etcd
